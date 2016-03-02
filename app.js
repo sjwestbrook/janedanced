a5b32fa95e829cb3a7a79b539b6574c51fe45386
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-03-02 08:49:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+var myApp = angular.module('myApp', ['ui.router']);
 
+myApp.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+  
+  $stateProvider 
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home.html'
+    })
+    
+    .state('about', {
+      url: '/about',
+      templateUrl: 'about.html'
+    })
+    
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'contact.html'
+    })
+    
+    .state('events', {
+      url: '/events',
+      templateUrl: 'events.html'
+    })
+    
+    .state('shelters', {
+      url: '/shelters',
+      templateUrl: 'shelters.html'
+    })
+    
+    .state('shop', {
+      url: '/shop',
+      templateUrl: 'shop.html'
+    })
+    
+    .state('vision', {
+      url: '/vision',
+      templateUrl: 'vision.html'
+    })
+});
